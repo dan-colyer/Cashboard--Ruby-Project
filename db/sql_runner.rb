736957812a70cd4c6f277eceb1abad7cd6844e68
dan <dancolyer@icloud.com>
2018-01-23 12:30:22 +0000

    Added DB details

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,11 @@ class SqlRunner
 
   def self.run( sql, values )
     begin
-      db = PG.connect({ dbname: 'money_cashboard', host: 'localhost' })
+      db = PG.connect({
+        dbname: 'd92mmsgpqg1pke',
+        user: 'nbkmpoqvokxdoq',
+        host: 'ec2-54-163-236-188.compute-1.amazonaws.com',
+        password: 'bf6fd84aa641d8222f1ee9f3df49137861555f4d6e4e5d5b7d3a4c6e9bbdbc93' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
